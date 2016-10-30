38a68e33208497f43f475e4a2a52ec2280f5aa74
craexus <craexus@users.noreply.github.com>
2016-10-30 12:57:09 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,39 @@
+/**
+ * Created by alex-acerpc on 2016-10-27.
+ */
+
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String args[]){
+        ScanF scanf = new ScanF();
+
+        System.out.println("Hello world");
+        //readStream();
+        //deviation();
+    }
+}
+
+/**
+private static int deviation(int n , float tRef, float tMon ){
+    int keyStroke;
+
+
+
+}
+
+ **/
+
+
+/**
+public static void readStream(){
+    FileInputStream file = new FileInputStream();
+
+
+}
+
+ **/
