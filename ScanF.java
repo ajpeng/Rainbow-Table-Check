a4f66586c6c440ac2a2ebf44b86e479957761f48
craexus <craexus@users.noreply.github.com>
2016-10-31 14:52:12 -0400

    Refractor code

--- ScanF.java
+++ ScanF.java
@@ -1,35 +1,43 @@
+/**
+ * Created by alex-acerpc on 2016-10-27.
+ */
+
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.Scanner;
 
-/**
- * Created by alex-acerpc on 2016-10-29.
- */
-public class ScanF{
-    public ScanF(){
-        try {
-            String curDir = System.getProperty("user.dir");
-            System.out.println(curDir);
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Input filename");
-            File file = new File(scanner.nextLine());
-            scanner = new Scanner(file);
-            for(int i = 0 ; i < 3 ; i++ ) {
-                String firstLine = scanner.nextLine();
-                System.out.println(firstLine);
-            }
-            int j=0;
-            while(scanner.hasNext()){
-                String line = scanner.nextLine();
-                //System.out.println(line);
-                j++;
-            }
-            //System.out.printf()
-            System.out.println(j);
-            scanner.close();
-        }
+public class Main extends ScanF {
 
-        catch(Exception e){
-            e.printStackTrace();
-        }
+
+    public static void main(String args[]){
+        ScanF scanf = new ScanF();
+        Scanner scanner = new Scanner(System.in);
+        String filename = scanner.nextLine();
+        File file = new File(filename);
+        createFlyArray(file);
+        createFirstDwellArray(file);
+        createSecondDwellArray(file);
+        getT(createSecondDwellArray(file));
     }
 }
+
+/**
+private static int deviation(int n , float tRef, float tMon ){
+    int keyStroke;
+
+
+
+}
+
+ **/
+
+
+/**
+public static void readStream(){
+    FileInputStream file = new FileInputStream();
+
+
+}
+
+ **/
